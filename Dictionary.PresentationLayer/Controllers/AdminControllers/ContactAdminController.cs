0f03ce0f41883e331a652010f822c9e188153c8b
Yahya Erdogan <yahyacanerdogan@outlook.com>
2024-03-20 10:21:40 -0500

    Worken for contact.

---
+++ Dictionary.PresentationLayer/Controllers/AdminControllers/ContactAdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dictionary.PresentationLayer.Controllers.AdminControllers
+{
+    public class ContactAdminController : Controller
+    {
+        // GET: ContactAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Compose()
+        {
+            return View();
+        }
+
+        public ActionResult ReadMail()
+        {
+            return View();
+        }
+    }
+}
