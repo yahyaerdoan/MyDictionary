464878dfd82ebf1edca78be31074d754c86e7451
Yahya Erdogan <yahyacanerdogan@outlook.com>
2024-03-20 19:37:59 -0500

    Compose page added. Bugs fixed.

--- Dictionary.PresentationLayer/Controllers/AdminControllers/MessageAdminController.cs
+++ Dictionary.PresentationLayer/Controllers/AdminControllers/MessageAdminController.cs
@@ -1,4 +1,5 @@
 ﻿using Dictionary.BussinessLogicLayer.Abstract;
+using Dictionary.EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +11,36 @@ namespace Dictionary.PresentationLayer.Controllers.AdminControllers
     public class MessageAdminController : Controller
     {
         private readonly IMessageService _messageService;
+
+        public MessageAdminController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
         // GET: MessageAdmin
         public ActionResult Index()
         {
-            var values = _messageService.TGetAllList();
+            var values = _messageService.TListByFilter(a=> a.ReceverMail == "yahyajohn@gmail.com");
             return View(values);
+        }
+
+        public ActionResult SentBox()
+        {
+            var values = _messageService.TListByFilter(a => a.SenderMail == "yahyajohn@gmail.com");
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreateMessage()
+        {            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateMessage(Message  message)
+        {
+            _messageService.TAdd(message);
+            return RedirectToAction("SentBox");
         }
     }
 }
