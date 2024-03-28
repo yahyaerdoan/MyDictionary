3a0f3c5c609022b277a2981b01bd5512d8345889
Yahya Erdogan <yahyacanerdogan@outlook.com>
2024-03-28 15:08:48 -0500

    Template added. Route bugs fixed.

---
+++ Dictionary.PresentationLayer/Controllers/WriterControllers/MyContentController.cs
@@ -0,0 +1,26 @@
+﻿using Dictionary.BussinessLogicLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dictionary.PresentationLayer.Controllers.WriterControllers
+{
+    public class MyContentController : Controller
+    {
+        private readonly IContentService _contentService;
+
+		public MyContentController(IContentService contentService)
+		{
+			_contentService = contentService;
+		}
+
+		// GET: MyContent
+		public ActionResult Index()
+        {
+            var values = _contentService.TListByFilter(a => a.WriterId == 7);
+            return View(values);
+        }
+    }
+}
---
+++ Dictionary.PresentationLayer/Controllers/WriterControllers/MyLogInController.cs
@@ -0,0 +1,26 @@
+﻿using Dictionary.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dictionary.PresentationLayer.Controllers.WriterControllers
+{
+    public class MyLogInController : Controller
+    {
+        // GET: MyLogIn
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Writer writer)
+        {
+
+            return View();
+        }
+    }
+}
